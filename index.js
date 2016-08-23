ae0aede115eb21f152b4cb0245714322ee2e9abc
Farbod <farbodsaraf@gmail.com>
2016-08-23 21:34:00 +0200

    up and running

--- index.js
+++ index.js
@@ -26,6 +26,22 @@ app.get('/webhook/', function (req, res) {
     res.send('Error, wrong token')
 })
 
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAE6DbdkqO8BADTPLEOimoELdktUtQqZCKyZBBfqUKZCq9Fsn6RExEt8ZATbHocLn1JWS0hYsWGMEHe2Y35ka2PMXou4ARnSqUa8vIZBaZBZCw0AAe0WwxEGrIm21bZCpCr4i5ZCcdlZBKKjrxxoDZCTqPMZC0nHrB4s84OhUzZBvRBlyQQZDZD"
+
+
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
